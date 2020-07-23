5965a3bf3fe9fd5db415629c9a573d4cf63fc62b
slqpez <santiago.lopez16@udea.edu.co>
2020-07-23 10:36:04 -0500

    LocalStorage.

---
+++ src/js/app.js
@@ -0,0 +1,22 @@
+const textarea = document.querySelector("#textarea");
+const button = document.querySelector("#btn__task");
+const list = document.querySelector(".task-list");
+
+button.addEventListener("click", (e) => {
+  const task = textarea.value;
+  let sizeStorage = localStorage.length;
+  localStorage.setItem(`task ${sizeStorage + 1}`, `${task}`);
+
+  console.log(sizeStorage);
+  list.innerHTML += `<li class="task"> ${task}</li>`;
+  textarea.value = "";
+  e.preventDefault();
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  for (let i = 0; i <= localStorage.length - 1; i++) {
+    const key = localStorage.key(i);
+    console.log(key);
+    list.innerHTML += `<li class="task"> ${localStorage.getItem(key)}</li>`;
+  }
+});
