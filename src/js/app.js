10d8b58d93317139e438d8ca3ae2e651b36f6a7f
slqpez <santiago.lopez16@udea.edu.co>
2020-07-23 19:21:07 -0500

    Falta borrar del localStorage.

--- src/js/app.js
+++ src/js/app.js
@@ -2,20 +2,44 @@ const textarea = document.querySelector("#textarea");
 const button = document.querySelector("#btn__task");
 const list = document.querySelector(".task-list");
 
-button.addEventListener("click", (e) => {
-  const task = textarea.value;
-  let sizeStorage = localStorage.length;
-  localStorage.setItem(`task ${sizeStorage + 1}`, `${task}`);
-  list.innerHTML += `<li class="task"> ${task}</li>`;
-  textarea.value = "";
-  e.preventDefault();
-});
+button.addEventListener("click", addTask);
+list.addEventListener("click", removeTask);
+document.addEventListener("DOMContentLoaded", loadTasks);
 
-document.addEventListener("DOMContentLoaded", () => {
+function addTask(e) {
+  const task = textarea.value;
+  const sizeStorage = localStorage.length;
+  if (task !== "") {
+    list.innerHTML += `<li class="task"> ${task}<a class="delete"></a></li>`;
+    localStorage.setItem(`task${sizeStorage + 1}`, `${task}`);
+    textarea.value = "";
+    e.preventDefault();
+  } else {
+    alert("No ingresaste ninguna tarea");
+  }
+}
+
+function removeTask(e) {
+  const erase = document.querySelector(".delete");
+  if (e.target.className === "delete") {
+    erase.parentElement.remove();
+    const test = erase.parentElement.className;
+    const toDelete = erase.parentElement.textContent;
+
+    for (let i = 0; i <= localStorage.length - 1; i++) {
+      const key = localStorage.key(i);
+      console.log(localStorage.getItem(key));
+      if (localStorage.getItem(key) === toDelete) console.log("estoy borrando");
+    }
+  }
+  e.preventDefault();
+}
+
+function loadTasks() {
   for (let i = 0; i <= localStorage.length - 1; i++) {
     const key = localStorage.key(i);
-    console.log(i);
-    console.log(key);
-    list.innerHTML += `<li class="task"> ${localStorage.getItem(key)}</li>`;
+    list.innerHTML += `<li class="task"> ${localStorage.getItem(
+      key
+    )}<a class="delete"></a></li>`;
   }
-});
+}
